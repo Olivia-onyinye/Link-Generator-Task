2569f0c947d5b8f401c0619770e0a28a78865d79
Olivia-onyinye <n.oliviaonyinye@gmail.com>
2024-03-25 19:31:28 +0100

    created test cases

---
+++ src/test/java/com/company/demo/services/serviceImpl/TransactionServiceImplTest.java
@@ -0,0 +1,29 @@
+package com.company.demo.services.serviceImpl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void saveTransaction() {
+    }
+
+    @Test
+    void viewTransaction() {
+    }
+
+    @Test
+    void generateInvoiceViewLink() {
+    }
+
+    @Test
+    void generateInvoicePaymentLink() {
+    }
+}
